692101e9873e61bf99157cb34ed526f28175a3bf
Gunnar Olsen <thegunnarolsen@gmail.com>
2016-03-17 23:15:06 -0500

    Delete keys.py

--- keys.py
+++
@@ -1,6 +0,0 @@
-keys = dict(
-consumer_key = "YyGNPAilqnOb3zsTEGqxZ2zcP",
-consumer_secret = "Do2DALsPpKvcwzEO06SZcQ1Hy4coUCgIpGRkRRNPtMpCvKNbXv",
-access_token = "709150507692990464-ZkWCAWaydvR7haYEPQe3Vl1f05WKhsG",
-access_token_secret = "RsUNhJGH8JUd7SWUSE4EaMAf5uu43uFNG7rLeaXZpZpZF"
-)
